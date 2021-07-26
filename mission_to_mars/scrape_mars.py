cb697909a78e36543a7bc65bf21627bb2cadb3fd
cjdance <c.j.dancewilson@outlook.com>
2021-07-26 21:55:08 +1000

    Added all base files for the project and small amount of starter code.

---
+++ mission_to_mars/scrape_mars.py
@@ -0,0 +1,10 @@
+
+
+
+data = {
+    "news-title": news_title(),
+    "news-paragraph": news_paragraph(),
+    "featured_image": featured_image(),
+    "facts": mars_facts()
+    "hemispheres": hemispheres()
+}
